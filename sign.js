bad7ed154f0b6477ee48847ff6effcb4c6969b29
Ellie Cochran <sintelligentdesign@icloud.com>
2016-12-20 01:41:05 -0500

    Added date & update function

--- sign.js
+++ sign.js
@@ -2,6 +2,7 @@
 var d = new Date();
 
 var date = d.getDate()
+var month = d.getMonth()
 var day = d.getDay()
 var hours = d.getHours()
 var minutes = d.getMinutes()
@@ -16,16 +17,29 @@ if (hours > 12) {
 
 /// Clock
 function updateClock() {
-    // Format hours and minutes
     minutes = (minutes < 10 ? "0" : "") + minutes
 
     hours = (hours > 12) ? hours - 12: hours
     hours = (hours == 0) ? 12 : hours
 
-    var meridian = (hours < 12) ? "AM" : "PM"
+    var meridian = (hours < 12) ? "am" : "pm"
 
     var timeString = hours + ":" + minutes + " " + meridian
 
-    // TODO: change to set innerHTML
     document.getElementById("time").innerHTML = timeString
+}
+
+/// Date
+function updateDate() {
+    var weekdayString = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+    var monthString = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+    document.getElementById("date").innerHTML = weekdayString[day] + " " + monthString[month] + " " + date
+}
+
+function main() {
+    updateClock()
+    updateDate()
+    setInterval(updateClock, 1000)
+    setInterval(updateDate, 1000)
 }
