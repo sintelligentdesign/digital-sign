4d65433e3ef5b7d1fede35545968258b2790dbc1
Ellie Cochran <sintelligentdesign@icloud.com>
2016-12-26 23:03:51 -0500

    design tweaks

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,8 @@ function updateWeather() {
         location: '17603',
         unit: 'f',
         success: function(weather) {
-            document.getElementById("weather-temp").innerHTML = weather.temp + '&deg;' + " " + weather.currently
+            document.getElementById("weather-temp").innerHTML = weather.temp + '&deg;'
+            document.getElementById("weather-condition").innerHTML = weather.currently
             document.getElementById("weather-highLow").innerHTML = weather.high + '&deg;' + " - " + weather.low + '&deg;'
         },
         error: function(error) {
