c87120d57854378393beb1d9df047c41ec49c8f3
Ellie Cochran <sintelligentdesign@icloud.com>
2017-06-19 16:11:42 -0400

    news works

--- js/news.js
+++ js/news.js
@@ -1,4 +1,16 @@
 function updateNews() {
     document.getElementById("news-items").innerText = ''
-    $('#news-items').rss("https://daringfireball.net/feeds/main")
+    $('#news-items').rss("http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml", {
+        // Number of entries
+        limit: 4,
+
+        // Display effect
+        effect: 'slideFastSynced',
+
+        // Template for layout
+        layoutTemplate: "{entries}",
+
+        // Template for entries
+        entryTemplate: "<div>{title}</div>"
+    })
 }
