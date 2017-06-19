43cf4748cc56cddc6d9b32a35c22142b00f2ece7
Ellie Cochran <sintelligentdesign@icloud.com>
2017-06-19 15:20:37 -0400

    Rearranged stuff

--- js/time.js
+++ js/time.js
@@ -2,24 +2,23 @@
 function updateClock() {
     var d = new Date()
     var hours = d.getHours()
+    console.log(hours)
     var minutes = d.getMinutes()
-    
-    var hoursString = new String
-    var minutesString = new String
-    var meridian = new String
 
-    if (hours > 11) {
-        hoursString = hours - 12
-    } else if (hours = 0) {
-        hoursString = 12
-    } else {
-        hoursString = hours
+    if (hours == 0) {
+        var hoursString = 12
+    }
+    if (hours > 12) {
+        var hoursString = hours - 12
+    }  
+    else if (hours != 0 && hours <= 12){
+        var hoursString = hours
     }
 
     if (hours < 12) {
-        meridian = "AM"
+        var meridian = "AM"
     } else {
-        meridian = "PM"
+        var meridian = "PM"
     }
 
     document.getElementById("time").innerHTML = hoursString + ":" + (minutes < 10 ? "0" : "") + minutes + "<span class='meridian'>" + meridian + "</span>"
